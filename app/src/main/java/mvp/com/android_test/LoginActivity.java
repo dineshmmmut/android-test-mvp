7536e372de2d4a632d5842f2e0477bf527ad80f2
nexogen_shared <testaccmyname@gmail.com>
2018-02-26 12:54:01 +0530

    init commit

--- app/src/main/java/mvp/com/android_test/LoginActivity.java
+++ app/src/main/java/mvp/com/android_test/LoginActivity.java
@@ -42,7 +42,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     /**
      * Id to identity READ_CONTACTS permission request.
      */
-    private static final int REQUEST_READ_CONTACTS = 0;
 
     /**
      * A dummy authentication store containing known user names and passwords.
